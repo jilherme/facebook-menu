1a343a2c9853e128a1513edb1e16b0a39b233aee
jilherme <guilhermerosa.sg094@gmail.com>
2021-06-18 14:34:28 -0300

    configs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import { icons } from "./assets/icons/";
-import { CSSTransition } from "react-transission-group";
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
   return (
@@ -16,13 +16,16 @@ function App() {
         <DropdownMenu />
       </NavItem>
     </Navbar>
-  );
+  );  
 }
 
 function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main'); //settings, animals 
+
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu )}>
         <span className="icon-button">{props.leftIcon}</span>
 
         {props.children}
@@ -34,11 +37,25 @@ function DropdownMenu() {
 
   return (
     <div className="dropdown">
-      <DropdownItem>My profile</DropdownItem>
-      <DropdownItem
-        leftIcon={<icons.Cog />}
-        rightIcon={<icons.Chevron />}
-      ></DropdownItem>
+      <CSSTransition in={activeMenu === "main"} unmountOnExit timeout={500} classNames="menu-primary"> {/* when the prop "in" is truthy, it'll render the animated children | unmountOnExit will remove this children when they're not active | timeout defines the duration of the animation.*/}
+        <div className={"menu"}>
+        <DropdownItem>My profile</DropdownItem>
+        <DropdownItem
+          leftIcon={<icons.Cog />}
+          rightIcon={<icons.Chevron />}
+        >Settings</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition in={activeMenu === "settings"} unmountOnExit timeout={500} classNames="menu-secondary"> {/* when the prop "in" is truthy, it'll render the animated children | unmountOnExit will remove this children when they're not active | timeout defines the duration of the animation.*/}
+        <div className={"menu"}>
+        <DropdownItem>My profile</DropdownItem>
+        <DropdownItem
+          leftIcon={<icons.Cog />}
+          rightIcon={<icons.Chevron />}
+        ></DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
